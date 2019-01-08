4c875e81de13badc9785bbf5883cffbf3d48e6df
foxcles <hitergod@163.com>
2019-01-08 20:25:10 +0800

    2019/1/8 foxcles
    1.first commit;

---
+++ src/template/tool-bar/search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="input-group">
+      <input type="text" class="form-control">
+      <span class="input-group-addon"><i class="fa fa-check"></i></span>
+    </div>
+    <br>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "search"
+    }
+</script>
+
+<style scoped>
+
+</style>
